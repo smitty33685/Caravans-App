15cbe0a4a40334ee9637ae0a79f534a5ec329e19
Jiří Smitek <jiri.smitek@juicymo.cz>
2022-05-03 17:08:05 +0200

    Project init

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { Heading } from "../src/components/LayoutComponents";
+
+const Home = () => {
+  return (
+    <PageWrapper>
+      <Heading>Prague Labs testovací zadání</Heading>
+    </PageWrapper>
+  );
+};
+
+const PageWrapper = styled.div``;
+
+export default Home;
